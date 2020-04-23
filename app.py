a7b68d59000f3c773e980ac0773b86f93d3d0c7d
yousafraza39142 <31831225+yousafraza39142@users.noreply.github.com>
2020-04-23 15:07:21 +0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,124 @@
+from flask import Flask
+import json
+from flask import jsonify, abort, request
+app = Flask(__name__)
+
+
+# import logging
+#
+# log = logging.getLogger('werkzeug')
+# log.setLevel(logging.ERROR)
+
+
+@app.route('/students')
+def students():
+    with open('data.json') as json_file:
+        data = json.load(json_file)
+
+    if request.method == "GET":
+        json_file.close()
+        return jsonify(data["data"])
+
+
+@app.route('/students/<stu_id>')
+def student_by_id(stu_id):
+    with open('data.json') as json_file:
+        data = json.load(json_file)
+    global id
+    try:
+        id = int(stu_id)
+    except:
+        abort(400)
+    for d in data["data"]:
+        if (d["id"] == id):
+            json_file.close()
+            return d
+    json_file.close()
+    abort(404)
+
+
+@app.route('/students/post')
+def studentsAdd():
+    with open('data.json') as json_file:
+        data = json.load(json_file)
+    currentId = int(data["id"])
+
+    print(request.json)
+    jsonObj = json.dumps(request.json, indent=2)
+    print(data)
+    dat = data["data"]
+    jsonObj = request.json
+    jsonObj["id"] = currentId
+    currentId += 1
+    dat.append(jsonObj)
+    data["data"] = dat
+    data["id"] = currentId
+    print(data)
+    json_file.close()
+    with open('data.json', 'w') as json_file:
+        json.dump(data, json_file)
+
+    json_file.close()
+
+    return jsonify("done")
+
+
+@app.route('/students/delete/<stu_id>')
+def deleteStudent(stu_id):
+    id = int(stu_id)
+    with open('data.json') as json_file:
+        data = json.load(json_file)
+
+    # del
+    print(type(data["data"]))
+
+    index = 0
+    for std in data["data"]:
+        if std["id"] == id:
+            del data["data"][index]
+        index += 1
+
+    print(data)
+    json_file.close()
+
+    with open('data.json', 'w') as json_file:
+        json.dump(data, json_file)
+
+    json_file.close()
+
+    return jsonify("done")
+
+
+@app.route('/students/update/<stu_id>')
+def UpdateStd(stu_id):
+    id = int(stu_id)
+    with open('data.json') as json_file:
+        data = json.load(json_file)
+
+    # del
+    newData = request.json
+
+    index = 0
+    for std in data["data"]:
+        if std["id"] == id:
+            if newData["name"] != "":
+                data["data"][index]["name"] = newData["name"]
+            if newData["email"] != "":
+                data["data"][index]["email"] = newData["email"]
+            if newData["year"] is not None:
+                data["data"][index]["year"] = newData["year"]
+        index += 1
+
+    print(data)
+    json_file.close()
+
+    with open('data.json', 'w') as json_file:
+        json.dump(data, json_file)
+
+    json_file.close()
+
+    return jsonify("done")
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ main.py
@@ -0,0 +1,77 @@
+from time import sleep
+
+import requests
+from os import system
+
+inp = 0
+
+while (inp != 6):
+    system("cls")
+    print(
+        "--------------------\n1)Read all students \n2)Get student by id\n3)Add student\n4)Edit student\n5)Remove student\n6)Exit\n----------------")
+    inp = int(input('Input:'))
+    if (inp == 1):
+        url_server = "http://127.0.0.1:5000/students"
+        r = requests.get(url=url_server)
+        data = r.json()
+        for std in data:
+            print("Student: Name=", std["name"], "Email=", std["email"], "year=", std["year"], "ID=", std["id"])
+        input("Any key to continue")
+    elif inp == 2:
+        id = input("give ID:")
+        url_server = "http://127.0.0.1:5000/students/" + id
+        r = requests.get(url=url_server)
+        std = r.json()
+        print("---------------------------\n", "Student: Name=", std["name"], "Email=", std["email"], "year=",
+              std["year"], "ID=", std["id"])
+        input("\n\nEnter to continue")
+    elif inp == 3:
+        name = input("*)Give Name:")
+        email = input("*)Give email:")
+        year = int(input("*)Give year:"))
+        url_server = "http://127.0.0.1:5000/students/post"
+        myda = {
+            "name": name,
+            "email": email,
+            "year": year
+        }
+        headers = {'content-type': 'application/json'}
+
+        r = requests.get(url=url_server, json=myda, headers=headers)
+        print("\n==>Student Added: Name=", name, "Email=", email, "year=",
+              year, "ID=", id)
+
+        input("\n\nPress Enter to continue")
+
+    elif inp == 4:
+        id = input("*)Give ID:")
+        name = input("*)Give Name(Enter to remain as before):")
+        email = input("*)Give email(Enter to remain as before):")
+        try:
+            year = int(input("*)Give year(Enter to remain as before):"))
+        except:
+            year = 0
+
+        url_server = "http://127.0.0.1:5000/students/update/" + id
+        myda = {
+            "name": name,
+            "email": email,
+            "year": year
+        }
+        headers = {'content-type': 'application/json'}
+
+        r = requests.get(url=url_server, json=myda, headers=headers)
+        print(r.json())
+    elif inp == 5:
+        id = input("Give ID:")
+
+        url_server = "http://127.0.0.1:5000/students/delete/" + id
+        r = requests.get(url=url_server)
+        print(r.json())
+    elif inp == 6:
+        system("cls")
+        print("Addios")
+        sleep(1)
+        exit(0)
+    else:
+        print("Wrong Input Enter Again")
